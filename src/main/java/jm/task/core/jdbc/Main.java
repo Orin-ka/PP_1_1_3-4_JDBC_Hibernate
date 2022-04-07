ee05e6b7d8a1a1e66a1778d9061a4471d9291d87
Оксана <ok.antonova@rambler.ru>
2022-04-07 22:58:43 +0300

    есть продвижки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        //String query = "USE test";
+        String query = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, \n" +
+                "name VARCHAR(80), lastname VARCHAR(80), age TINYINT);";
+
+        String queryInsertUser1 = "INSERT INTO users(name, lastname, age) VALUES ('Alex', 'Marshall', '18' );";// +
+                //String queryInsertUser1 "INSERT INTO users(name, lastname, age) VALUES ('Olga', 'Ivanova', '31' );" +
+               // "INSERT INTO users(name, lastname, age) VALUES ('Bill', 'Wells', '21' );" +
+               // "INSERT into users(name, lastname, age) VALUES ('Antonio', 'Banderas', '58' );";
+        try (Connection connection = Util.getConnection()) {
+
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            System.out.println("Таблица users создана");
+            statement.execute(queryInsertUser1);
+            System.out.println("4 юзера добавлены в таблицу users");
+
+            //ResultSet resultSet = statement.executeQuery(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String urlDB = "jdbc:mysql://127.0.0.1:3306/test";
+    private final static String user = "root";
+    private final static String password = "root";
+
+    public static Connection getConnection () {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(urlDB, user, password);
+            System.out.println("Подключение к БД выполнено");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
